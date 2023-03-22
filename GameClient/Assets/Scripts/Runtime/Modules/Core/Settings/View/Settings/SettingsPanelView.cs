d1c5b96fb60ad5b6464eb2f2e6ca1c620a9890d9
TayfurSafakGencay <safak.gencay@gmail.com>
2023-03-22 16:55:38 +0300

    tba

--- GameClient/Assets/Scripts/Runtime/Modules/Core/Settings/View/Settings/SettingsPanelView.cs
+++ GameClient/Assets/Scripts/Runtime/Modules/Core/Settings/View/Settings/SettingsPanelView.cs
@@ -1,11 +1,9 @@
 using StrangeIoC.scripts.strange.extensions.mediation.impl;
-using TMPro;
 
 namespace Runtime.Modules.Core.Settings.View.Settings
 {
   public class SettingsPanelView : EventView
   {
-    public TextMeshProUGUI textMeshProUGUI;
     public void OnClosePanel()
     {
       dispatcher.Dispatch(SettingsPanelEvent.ClosePanel);
