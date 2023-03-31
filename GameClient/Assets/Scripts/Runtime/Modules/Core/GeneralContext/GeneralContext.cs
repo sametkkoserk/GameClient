db0a5e94cd783a702c3c99fa17c7e890da6caca4
TayfurSafakGencay <safak.gencay@gmail.com>
2023-04-01 02:41:59 +0300

    tba

--- GameClient/Assets/Scripts/Runtime/Modules/Core/GeneralContext/GeneralContext.cs
+++ GameClient/Assets/Scripts/Runtime/Modules/Core/GeneralContext/GeneralContext.cs
@@ -1,5 +1,4 @@
 using Runtime.Contexts.Main.View.SettingsButton;
-using Runtime.Modules.Core.Localization.Model.LocalizationModel;
 using Runtime.Modules.Core.ScreenManager.Model.ScreenManagerModel;
 using Runtime.Modules.Core.ScreenManager.View.LayerContainer;
 using Runtime.Modules.Core.ScreenManager.View.PanelContainer;
@@ -24,18 +23,14 @@ namespace Runtime.Modules.Core.GeneralContext
     protected override void mapBindings()
     {
       base.mapBindings();
-
       injectionBinder.Bind<IScreenManagerModel>().To<ScreenManagerModel>().ToSingleton();
-
-      injectionBinder.Bind<ILocalizationModel>().To<LocalizationModel>().ToSingleton();
-
 
       mediationBinder.Bind<LayerContainerView>().To<LayerContainerMediator>();
       mediationBinder.Bind<PanelContainerView>().To<PanelContainerMediator>();
-
+      
       mediationBinder.Bind<SettingsButtonView>().To<SettingsButtonMediator>();
       mediationBinder.Bind<SettingsPanelView>().To<SettingsPanelMediator>();
-
+      
       mediationBinder.Bind<LanguageSettingsView>().To<LanguageSettingsMediator>();
     }
   }
