c6b1a8f4223cac77c4b15315f822f403ac54166f
TayfurSafakGencay <safak.gencay@gmail.com>
2023-04-01 03:02:11 +0300

    New: Starter settings command added.

---
+++ GameClient/Assets/Scripts/Runtime/Contexts/Main/Command/StarterSettingsCommand.cs
@@ -0,0 +1,34 @@
+using Runtime.Modules.Core.Cursor.Enum;
+using Runtime.Modules.Core.Cursor.Model.CursorModel;
+using Runtime.Modules.Core.Localization.Enum;
+using Runtime.Modules.Core.Localization.Model.LocalizationModel;
+using StrangeIoC.scripts.strange.extensions.command.impl;
+using StrangeIoC.scripts.strange.extensions.injector;
+
+namespace Runtime.Contexts.Main.Command
+{
+  public class StarterSettingsCommand : EventCommand
+  {
+    [Inject]
+    public ILocalizationModel localizationModel { get; set; }
+    
+    [Inject]
+    public ICursorModel cursorModel { get; set; }
+    public override void Execute()
+    {
+      LanguageSettings();
+      CursorSettings();
+    }
+
+    private void CursorSettings()
+    {
+      cursorModel.OnChangeCursor(CursorKey.Default);
+    }
+
+    private void LanguageSettings()
+    {
+      // In the future it will be from database or player pref.
+      localizationModel.ChangeLanguage(LanguageKey.en);
+    }
+  }
+}
