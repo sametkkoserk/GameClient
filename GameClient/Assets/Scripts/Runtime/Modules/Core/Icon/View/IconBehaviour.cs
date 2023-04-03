d48f232663db579d1bd54315b6abe709a734f7bf
TayfurSafakGencay <safak.gencay@gmail.com>
2023-04-03 13:10:18 +0300

    New: Color choices added to icon behaviour system.

--- GameClient/Assets/Scripts/Runtime/Modules/Core/Icon/View/IconBehaviour.cs
+++ GameClient/Assets/Scripts/Runtime/Modules/Core/Icon/View/IconBehaviour.cs
@@ -1,5 +1,6 @@
 using Runtime.Modules.Core.Icon.Enum;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.U2D;
 using UnityEngine.UI;
 
@@ -8,34 +9,37 @@ namespace Runtime.Modules.Core.Icon.View
   [RequireComponent(typeof(Image))]
   public class IconBehaviour : MonoBehaviour
   {
+    public Color color = Color.white;
+    [FormerlySerializedAs("IconKey")]
     [Space]
-    public IconKey IconKey;
+    public IconKey iconKey;
 
     private Image image;
 
     private SpriteAtlas spriteAtlas;
-
     private void OnEnable()
     {
       //Load a text file (Assets/Resources/Icon/IconKey.)
       image = gameObject.GetComponent<Image>();
       spriteAtlas = Resources.Load<SpriteAtlas>("Icon/IconSpriteAtlas");
-
-      image.sprite = spriteAtlas.GetSprite(IconKey.ToString());
+      
+      Init(iconKey, color);
     }
 
-    public void Init(IconKey iconKey)
+    public void Init(IconKey newIconKey, Color? newColor = null)
     {
-      IconKey = iconKey;
-      image.sprite = spriteAtlas.GetSprite(IconKey.ToString());
+      image.color = newColor ?? Color.white;
+      iconKey = newIconKey;
+      image.sprite = spriteAtlas.GetSprite(iconKey.ToString());
     }
 #if UNITY_EDITOR
     public void SetFromInspector()
     {
       image = gameObject.GetComponent<Image>();
       spriteAtlas = Resources.Load<SpriteAtlas>("Icon/IconSpriteAtlas");
-
-      image.sprite = spriteAtlas.GetSprite(IconKey.ToString());
+      
+      image.color = color;
+      image.sprite = spriteAtlas.GetSprite(iconKey.ToString());
     }
 #endif
   }
