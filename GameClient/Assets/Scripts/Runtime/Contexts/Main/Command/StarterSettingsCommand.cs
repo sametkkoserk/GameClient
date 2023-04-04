88b95a625535d071676f96e3d87180fd49b6d541
TayfurSafakGencay <safak.gencay@gmail.com>
2023-04-04 22:08:42 +0300

    New: Starter Settings adjusted.

--- GameClient/Assets/Scripts/Runtime/Contexts/Main/Command/StarterSettingsCommand.cs
+++ GameClient/Assets/Scripts/Runtime/Contexts/Main/Command/StarterSettingsCommand.cs
@@ -1,3 +1,5 @@
+using Runtime.Modules.Core.ColorPalette.Enum;
+using Runtime.Modules.Core.ColorPalette.Model.ColorPaletteModel;
 using Runtime.Modules.Core.Cursor.Enum;
 using Runtime.Modules.Core.Cursor.Model.CursorModel;
 using Runtime.Modules.Core.Localization.Enum;
@@ -14,9 +16,13 @@ namespace Runtime.Contexts.Main.Command
     
     [Inject]
     public ICursorModel cursorModel { get; set; }
+    
+    [Inject]
+    public IColorPaletteModel colorPaletteModel { get; set; }
     public override void Execute()
     {
       LanguageSettings();
+      ColorPaletteSettings();
       CursorSettings();
     }
 
@@ -29,6 +35,11 @@ namespace Runtime.Contexts.Main.Command
     {
       // In the future it will be from database or player pref.
       localizationModel.ChangeLanguage(LanguageKey.en);
+    }
+
+    private void ColorPaletteSettings()
+    {
+      colorPaletteModel.ChangeColorPalette(ColorPaletteKey.Standard);
     }
   }
 }
