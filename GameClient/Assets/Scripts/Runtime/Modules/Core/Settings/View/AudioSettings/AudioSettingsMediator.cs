e983a75bd7d985f2873583023041bd4568bdbcd0
TayfurSafakGencay <safak.gencay@gmail.com>
2023-04-07 18:59:51 +0300

    New: Audio Settings

--- GameClient/Assets/Scripts/Runtime/Modules/Core/Settings/View/AudioSettings/AudioSettingsMediator.cs
+++ GameClient/Assets/Scripts/Runtime/Modules/Core/Settings/View/AudioSettings/AudioSettingsMediator.cs
@@ -1,18 +1,60 @@
-using strange.extensions.mediation.impl;
+using Runtime.Modules.Core.Audio.Model.AudioModel.AudioModel;
+using StrangeIoC.scripts.strange.extensions.injector;
+using StrangeIoC.scripts.strange.extensions.mediation.impl;
+using UnityEngine.PlayerLoop;
 
 namespace Runtime.Modules.Core.Settings.View.AudioSettings
 {
+  public enum AudioSettingsEvent
+  {
+    MasterSliderValueChanged,
+    MusicSliderValueChanged,
+    UISliderValueChanged,
+  }
   public class AudioSettingsMediator : EventMediator
   {
     [Inject]
-    public VIEW view { get; set; }
+    public AudioSettingsView view { get; set; }
+    
+    [Inject]
+    public IAudioModel audioModel { get; set; }
 
     public override void OnRegister()
     {
+      view.dispatcher.AddListener(AudioSettingsEvent.MasterSliderValueChanged, OnMasterVolumeChanged);
+      view.dispatcher.AddListener(AudioSettingsEvent.MusicSliderValueChanged, OnMusicVolumeChanged);
+      view.dispatcher.AddListener(AudioSettingsEvent.UISliderValueChanged, OnUIVolumeChanged);
+
+      Init();
+    }
+
+    private void Init()
+    {
+      view.masterVolumeSlider.value = audioModel.masterVolume;
+      view.musicVolumeSlider.value = audioModel.musicVolume;
+      view.uiVolumeSlider.value = audioModel.uiVolume;
+    }
+
+    private void OnUIVolumeChanged()
+    {
+      audioModel.ChangeUISoundVolume(view.uiVolumeSlider.value);
+    }
+
+    private void OnMusicVolumeChanged()
+    {
+      audioModel.ChangeMusicVolume(view.musicVolumeSlider.value);
+    }
+
+    private void OnMasterVolumeChanged()
+    {
+      audioModel.ChangeMasterVolume(view.masterVolumeSlider.value);
     }
 
     public override void OnRemove()
     {
+      view.dispatcher.RemoveListener(AudioSettingsEvent.MasterSliderValueChanged, OnMasterVolumeChanged);
+      view.dispatcher.RemoveListener(AudioSettingsEvent.MusicSliderValueChanged, OnMusicVolumeChanged);
+      view.dispatcher.RemoveListener(AudioSettingsEvent.UISliderValueChanged, OnUIVolumeChanged);
     }
   }
 }
--- GameClient/Assets/Scripts/Runtime/Modules/Core/Settings/View/AudioSettings/AudioSettingsView.cs
+++ GameClient/Assets/Scripts/Runtime/Modules/Core/Settings/View/AudioSettings/AudioSettingsView.cs
@@ -1,9 +1,29 @@
-using strange.extensions.mediation.impl;
+using StrangeIoC.scripts.strange.extensions.mediation.impl;
+using UnityEngine.UI;
 
 namespace Runtime.Modules.Core.Settings.View.AudioSettings
 {
   public class AudioSettingsView : EventView
   {
+    public Slider masterVolumeSlider;
+
+    public void OnMasterVolumeChanged()
+    {
+      dispatcher.Dispatch(AudioSettingsEvent.MasterSliderValueChanged);
+    }
     
+    public Slider musicVolumeSlider;
+    
+    public void OnMusicVolumeChanged()
+    {
+      dispatcher.Dispatch(AudioSettingsEvent.MusicSliderValueChanged);
+    }
+    
+    public Slider uiVolumeSlider;
+
+    public void OnUIVolumeChanged()
+    {
+      dispatcher.Dispatch(AudioSettingsEvent.UISliderValueChanged);
+    }
   }
 }
