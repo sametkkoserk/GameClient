4713fa3f20a37ffec05b3b265da72eb3c335edb5
TayfurSafakGencay <safak.gencay@gmail.com>
2023-05-10 11:58:19 +0300

    New: Player model added.

---
+++ GameClient/Assets/Scripts/Runtime/Contexts/Main/Model/PlayerModel/PlayerModel.cs
@@ -0,0 +1,9 @@
+using Runtime.Contexts.Main.Vo;
+
+namespace Runtime.Contexts.Main.Model.PlayerModel
+{
+    public class PlayerModel : IPlayerModel
+    {
+        public PlayerRegisterInfoVo playerRegisterInfoVo { get; set; }
+    }
+}
