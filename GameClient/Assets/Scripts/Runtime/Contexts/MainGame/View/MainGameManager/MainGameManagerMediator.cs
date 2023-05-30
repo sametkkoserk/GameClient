cc3cde07453c87dc2bd028cfd6045700a208963d
TayfurSafakGencay <safak.gencay@gmail.com>
2023-05-30 12:13:40 +0300

    Bug Fixed: Data adjusted.

--- GameClient/Assets/Scripts/Runtime/Contexts/MainGame/View/MainGameManager/MainGameManagerMediator.cs
+++ GameClient/Assets/Scripts/Runtime/Contexts/MainGame/View/MainGameManager/MainGameManagerMediator.cs
@@ -1,14 +1,17 @@
 using System;
 using Riptide;
 using Runtime.Contexts.Lobby.Model.LobbyModel;
+using Runtime.Contexts.Main.Model.PlayerModel;
 using Runtime.Contexts.MainGame.Enum;
 using Runtime.Contexts.MainGame.Model;
 using Runtime.Contexts.MainGame.Vo;
 using Runtime.Contexts.Network.Enum;
 using Runtime.Contexts.Network.Services.NetworkManager;
 using Runtime.Contexts.Network.Vo;
+using Runtime.Modules.Core.Discord.Model;
 using Runtime.Modules.Core.ScreenManager.Enum;
 using Runtime.Modules.Core.ScreenManager.Model.ScreenManagerModel;
+using StrangeIoC.scripts.strange.extensions.context.api;
 using StrangeIoC.scripts.strange.extensions.dispatcher.eventdispatcher.api;
 using StrangeIoC.scripts.strange.extensions.injector;
 using StrangeIoC.scripts.strange.extensions.mediation.impl;
@@ -31,7 +34,20 @@ namespace Runtime.Contexts.MainGame.View.MainGameManager
 
     [Inject]
     public IScreenManagerModel screenManagerModel { get; set; }
+    
+    [Inject]
+    public IDiscordModel discordModel { get; set; }
+    
+    [Inject]
+    public IPlayerModel playerModel { get; set; }
 
+    public override void OnRegister()
+    {
+      dispatcher.AddListener(ServerToClientId.SendTurn, OnTurn);
+
+      dispatcher.AddListener(MainGameEvent.TurnTimeOver, TurnTimeOver);
+    }
+    
     private void Start()
     {
       GameStartVo vo = new()
@@ -41,21 +57,17 @@ namespace Runtime.Contexts.MainGame.View.MainGameManager
       };
       Message message = Message.Create(MessageSendMode.Reliable, (ushort)ClientToServerId.GameStart);
       message = networkManager.SetData(message, vo);
-
+      
       networkManager.Client.Send(message);
+      
+      discordModel.InGame(playerModel.playerRegisterInfoVo.username);
     }
-
-    public override void OnRegister()
-    {
-      dispatcher.AddListener(ServerToClientId.SendTurn, OnTurn);
-
-      dispatcher.AddListener(MainGameEvent.TurnTimeOver, TurnTimeOver);
-    }
-
+    
     private void OnTurn(IEvent payload)
     {
       MessageReceivedVo messageReceivedVo = (MessageReceivedVo)payload.data;
-      ushort inLobbyId = Convert.ToUInt16(messageReceivedVo.message);
+      ushort inLobbyId = Convert.ToUInt16(messageReceivedVo.message[1]);
+      
       mainGameModel.queue = inLobbyId;
 
       screenManagerModel.OpenPanel(MainGameKeys.YourTurnPanel, SceneKey.MainGame, LayerKey.FirstLayer, PanelMode.Destroy, PanelType.FullScreenPanel);
